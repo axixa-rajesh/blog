019a24b7b1a7aa9fdf2184c8ae2d3e9122aff03b
rmcats <rmcats@gmail.com>
2022-12-28 18:11:23 +0530

    some changes in heder file

--- apps/views/layout/header.php
+++ apps/views/layout/header.php
@@ -8,7 +8,7 @@
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarText">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0"> 
                             <?php
                             if ((Session::get('loginDtl'))) { ?>
                                 <li class="nav-item">
