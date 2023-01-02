f1cb19515af711cee9ca1173039d77d309b88aaa
Sumit Sethia <sumitjainsj1997@gmail.com>
2023-01-02 17:34:55 +0530

    giyanu master

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once "apps/libs/Session.php";
 Session::init();
 $allfiles=glob("helper/*.php");
-array_map(fn($fl)=>require_once($fl),$allfiles);
+array_map(fn($fl)=>require_once($fl),$allfiles);// jaii shiree ram
 spl_autoload_register(fn($clname)=>(file_exists("apps/libs/$clname.php"))?require_once "apps/libs/$clname.php":'');
 include "apps/libs/Loader.php";
 include "apps/libs/Controller.php";
